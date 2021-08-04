c1ffdd1aec37f60c6294df3e99bed91b9668c14e
vian_ofc <74562450+Alvian03@users.noreply.github.com>
2021-08-05 04:30:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ const mulai = async (sesi, conn = new WAConnection()) => {
           reconnect.stop()
           const isi = `[ ANCHT ] Started At ${moment().format('HH:mm:ss DD/MM/YYYY')}`
           if (ConnectingWs) {
-               conn.sendMessage(settings.Owner, isi, 'conversation', { quoted: { key: { remoteJid: "0@s.whatsapp.net", fromMe: false, }, message: { conversation: `*Created By MRHRTZ*`, }, } })
+               conn.sendMessage(settings.Owner, isi, 'conversation', { quoted: { key: { remoteJid: "0@s.whatsapp.net", fromMe: false, }, message: { conversation: `-[ Created ]-\nBy Vian ofc`, }, } })
           }
           IsShouldmessage = true
      })
